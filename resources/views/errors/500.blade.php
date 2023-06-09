f1d6ed4bd86a2e3199f9e3d2fc29a22dfe7c4bb7
@mhmamun6316 <mahedi.hasasan@softbdltd.com>
2023-06-10 04:01:57 +0600

    error handler page added

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hello, world!</title>
+    <title>500!</title>
 
     <style>
         body {
