fe18077c016aadf4351f4d0f3204fada423e3359
@mhmamun6316 <mahedi.hasasan@softbdltd.com>
2023-07-22 16:44:08 +0600

    controller commit

--- app/Http/Controllers/Admin/BillController.php
+++ app/Http/Controllers/Admin/BillController.php
@@ -9,7 +9,10 @@ use App\Models\Admin\Student;
 use App\Traits\FilterByOrganization;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Yajra\DataTables\DataTables;
+use PDF;
+use Dompdf\Options;
 
 class BillController extends Controller
 {
@@ -61,17 +64,27 @@ class BillController extends Controller
         }
 
         foreach ($organizations as $organization) {
+
+            $firstDayOfPreviousMonth = Carbon::now()->subMonth()->startOfMonth();
+            $lastDayOfPreviousMonth = Carbon::now()->subMonth()->endOfMonth();
+            $firstDayOfCurrentMonth = Carbon::now()->startOfMonth();
+            $lastDayOfCurrentMonth = Carbon::now()->endOfMonth();
+
             $students = $organization->students;
             $organizationDueAmount = 0;
 
             foreach ($students as $student) {
+
                 $assignments = $student->packages()
-                    ->where(function ($query) {
-                        $query->where('student_package.end_date', '<=', Carbon::now()->subMonth()->endOfMonth())
-                            ->orWhereNull('student_package.end_date')
-                            ->orWhere(function ($query) {
-                                $query->where('student_package.end_date', '>=', Carbon::now()->startOfMonth())
-                                    ->where('student_package.end_date', '<=', Carbon::now()->endOfMonth());
+                    ->where(function ($query) use ($firstDayOfPreviousMonth, $lastDayOfPreviousMonth, $firstDayOfCurrentMonth, $lastDayOfCurrentMonth) {
+                        $query->whereBetween('student_package.end_date',[$firstDayOfPreviousMonth, $lastDayOfPreviousMonth])
+                            ->orWhere(function ($query) use ($firstDayOfPreviousMonth, $lastDayOfPreviousMonth, $firstDayOfCurrentMonth, $lastDayOfCurrentMonth) {
+                                $query->whereBetween('student_package.start_date', [$firstDayOfPreviousMonth, $lastDayOfPreviousMonth])
+                                      ->whereBetween('student_package.end_date', [$firstDayOfCurrentMonth, $lastDayOfCurrentMonth]);
+                            })
+                            ->orWhere(function ($query) use ($firstDayOfCurrentMonth, $lastDayOfCurrentMonth) {
+                                $query->whereNotBetween('student_package.start_date', [$firstDayOfCurrentMonth, $lastDayOfCurrentMonth])
+                                      ->whereNull('student_package.end_date');
                             });
                     })
                     ->get();
@@ -108,11 +121,11 @@ class BillController extends Controller
                         }
                     }
 
-                    if ($startDate->weekOfMonth === 1) {
-                        $dueAmount += $packagePrice;
-                    } else {
-                        $dueAmount += $packagePrice * 0.5;
-                    }
+//                    if ($startDate->weekOfMonth === 1) {
+//                        $dueAmount += $packagePrice;
+//                    } else {
+//                        $dueAmount += $packagePrice * 0.5;
+//                    }
                 }
 
                 $organizationDueAmount += $dueAmount;
@@ -129,12 +142,97 @@ class BillController extends Controller
                 return '<span class="badge badge-round badge-primary badge-lg">' . $organizations->name . '</span>';
             })
             ->editColumn('dueAmount', function ($organizations){
-                return '<span class="badge badge-round badge-success badge-lg">' . $organizations->dueAmount . '</span>';
+                return '<span class="badge badge-round badge-danger badge-lg">' . $organizations->dueAmount . '</span>';
             })
             ->editColumn('students', function ($organizations){
-                return '<span class="badge badge-round badge-danger badge-lg">' . $organizations->students->count() . '</span>';
+                return '<span class="badge badge-round badge-success badge-lg">' . $organizations->students->count() . '</span>';
             })
-            ->rawColumns(['organization','dueAmount','students'])
+            ->addColumn('action', function ($organizations) {
+                $url = route('admin.organization.bill', ['id' => $organizations->id]);
+                return '<a href="' . $url . '" class="btn btn-sm btn-warning"><i class="fa fa-file-pdf"></i></a>';
+            })
+            ->rawColumns(['organization','dueAmount','students','action'])
             ->make(true);
+    }
+
+    public function organizationPdf($id){
+        $organization = Organization::findOrfail($id);
+
+        $students = $organization->students;
+        $firstDayOfPreviousMonth = Carbon::now()->subMonth()->startOfMonth();
+        $lastDayOfPreviousMonth = Carbon::now()->subMonth()->endOfMonth();
+        $firstDayOfCurrentMonth = Carbon::now()->startOfMonth();
+        $lastDayOfCurrentMonth = Carbon::now()->endOfMonth();
+
+        $totalDueAmount = 0;
+        $assignmentsArray = [];
+
+        foreach ($students as $student) {
+            $assignments = $student->packages()
+                ->where(function ($query) use ($firstDayOfPreviousMonth, $lastDayOfPreviousMonth, $firstDayOfCurrentMonth, $lastDayOfCurrentMonth) {
+                    $query->whereBetween('student_package.end_date', [$firstDayOfPreviousMonth, $lastDayOfPreviousMonth])
+                        ->orWhere(function ($query) use ($firstDayOfPreviousMonth, $lastDayOfPreviousMonth, $firstDayOfCurrentMonth, $lastDayOfCurrentMonth) {
+                            $query->whereBetween('student_package.start_date', [$firstDayOfPreviousMonth, $lastDayOfPreviousMonth])
+                                ->whereBetween('student_package.end_date', [$firstDayOfCurrentMonth, $lastDayOfCurrentMonth]);
+                        })
+                        ->orWhere(function ($query) use ($firstDayOfCurrentMonth, $lastDayOfCurrentMonth) {
+                            $query->whereNotBetween('student_package.start_date', [$firstDayOfCurrentMonth, $lastDayOfCurrentMonth])
+                                ->whereNull('student_package.end_date');
+                        });
+                })
+                ->get();
+
+            foreach ($assignments as $assignment) {
+                $startDate = Carbon::parse($assignment->pivot->start_date);
+                $endDate = Carbon::parse($assignment->pivot->end_date);
+
+                // Adjust the start date if it is earlier than the first date of the previous month
+                if ($startDate->lessThan($firstDayOfPreviousMonth)) {
+                    $startDate = $firstDayOfPreviousMonth;
+                }
+
+                // Adjust the end date if it is greater than the last date of the previous month
+                if ($endDate && $endDate->greaterThan($lastDayOfPreviousMonth)) {
+                    $endDate = $lastDayOfPreviousMonth;
+                }
+
+                // Calculate the number of days the package was used within the previous month
+                $daysInPreviousMonth = $startDate->diffInDays($endDate) + 1;
+
+                // Calculate the due amount based on the number of days used
+                $packagePrice = $assignment->price;
+
+                if ($daysInPreviousMonth > 1) {
+                    if ($daysInPreviousMonth <= 7) {
+                        $dueAmount = $packagePrice * 0.5;
+                    } else {
+                        $dueAmount = $packagePrice;
+                    }
+                } else {
+                    $dueAmount = 0;
+                }
+
+                // Add the assignment details to the assignmentsArray
+                $assignmentsArray[] = [
+                    'student_name' => $student->name,
+                    'package_name' => $assignment->name,
+                    'start_date' => $startDate->format('Y-m-d'),
+                    'end_date' => $endDate ? $endDate->format('Y-m-d') : null,
+                    'days_used' => $daysInPreviousMonth,
+                    'due_amount' => $dueAmount,
+                ];
+
+                $totalDueAmount += $dueAmount;
+            }
+        }
+
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial'); // Optional: You can set the default font
+
+        $pdf = PDF::loadView('admin.bill.bill_report', compact('organization', 'assignmentsArray', 'totalDueAmount'));
+
+        $prevMonth =  now()->subMonth()->format('F');
+
+        return $pdf->download('invoice_of_'.$prevMonth.'.pdf');
     }
 }
