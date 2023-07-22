e0627567c328e8f6abdcf8e1d535149be6a99a62
@mhmamun6316 <mahedi.hasasan@softbdltd.com>
2023-07-22 16:47:00 +0600

    database

--- database/migrations/2023_06_10_184643_create_students_table.php
+++ database/migrations/2023_06_10_184643_create_students_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('student_id')->unique();
             $table->string('avatar')->nullable();
             $table->string('phone')->nullable();
             $table->string('email')->nullable();
